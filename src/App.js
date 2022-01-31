a4ca9e5ffe3186bf19eb0aa35723f76a320436e4
j985chen <j985chen@uwaterloo.ca>
2022-01-30 22:30:05 -0500

    complete website overhaul

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import Book from './components/book';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons'
 import { faAngleLeft, faAngleRight, faEnvelope, faFileAlt } from '@fortawesome/free-solid-svg-icons';
@@ -9,13 +8,12 @@ library.add(faAngleLeft, faAngleRight, faEnvelope, faFileAlt, fab);
 function App() {
   return (
     <>
-        <Book />
         <div className='displayNotAvailable'>
             <p>
-                Sorry, this website doesn't support your screen's dimensions.
+                This website is currently under construction.
             </p> 
             <p>
-                Please try again with another device, and in the meantime, check out my <a href='http://github.com/j985chen'>GitHub</a> and <a href='http://linkedin.com/in/j985chen'>LinkedIn</a>.
+                In the meantime, please check out my <a href='http://github.com/j985chen'>GitHub</a> and <a href='http://linkedin.com/in/j985chen'>LinkedIn</a>!
             </p>
         </div>
     </>
